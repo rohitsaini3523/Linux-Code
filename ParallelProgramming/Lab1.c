d401da87dba3aa931f3db580e0a4ee5e4cea7a80
rohitsaini3523 <rohitsaini3523@gmail.com>
2022-03-29 00:00:39 +0530

    ok

--- ParallelProgramming/Lab1.c
+++ ParallelProgramming/Lab1.c
@@ -1,27 +1,81 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 #include<omp.h>
-
+#define MAX 40
+ 	
 int main(void)
 {
-    int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int b[3][3] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
-    int c[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-    int sum = 0;
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            for (int k = 0; k < 3; k++)
+	int count =1;	
+    int i,j,k;    
+    int a[MAX][MAX];
+    int b[MAX][MAX];
+    int c[MAX][MAX];
+	int c_p[MAX][MAX];
+
+	for (i = 0; i < MAX; i++)
+        for (j = 0; j < MAX; j++)
+	{
+	 a[i][j] = count;
+	b[i][j] =  count;
+	c[i][j] = 0;	
+	c_p[i][j] = 0;	
+	count++; 	
+	}
+
+printf("\nMatrix A:");
+	for (i = 0; i < MAX; i++){
+        for (j = 0; j < MAX; j++)
+	{
+		//printf("%d ",a[i][j]);
+	}//printf("\n");
+}
+printf("\nMatrix B:");
+for (i = 0; i < MAX; i++){
+        for (j = 0; j < MAX; j++)
+	{
+		//printf("%d ",b[i][j]);
+	}//printf("\n");
+}
+double start = omp_get_wtime();
+	printf("\nSerial Multiplication of Matrix: \n");
+	for (i = 0; i < MAX; i++)
+        for (j = 0; j < MAX; j++)
+            for (k = 0; k < MAX; k++)
             {
                 c[i][j] += a[i][k] * b[k][j];
             }
-    for (int i = 0; i < 3; i++)
+double stop = omp_get_wtime();
+	 for (int i = 0; i < MAX; i++)
+	    {
+		for (int j = 0; j < MAX; j++)
+		{
+		    //printf("%d ", c[i][j]);
+		    //printf(" ");
+		}
+		//printf("\n");
+	    }
+	printf("\nSerial Time: %f\n",stop-start);
+sleep(2);
+double start_p = omp_get_wtime();
+#pragma omp parallel for private(i,j,k)
+	for (i = 0; i < MAX; i++)
+        for (j = 0; j < MAX; j++)
+            for (k = 0; k < MAX; k++)
+            {
+                c_p[i][j] += a[i][k] * b[k][j];
+            }
+double stop_p = omp_get_wtime();
+printf("\nParallel Multiplication of Matrix: \n");
+    for (int i = 0; i < MAX; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < MAX; j++)
         {
-            printf("%d", c[i][j]);
-            printf(" ");
+            //printf("%d ", c_p[i][j]);
+            //printf(" ");
         }
-        printf("\n");
+        //printf("\n");
     }
+printf("\nParallel Time : %f\n",stop_p-start_p);
         return 0;
 }
