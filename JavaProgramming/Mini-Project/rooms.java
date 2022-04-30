a02279d4c449182cc7ca9cdce2ddcef886cb1f31
rohitsaini3523 <rohitsaini3523@gmail.com>
2022-04-30 11:52:23 +0530

    master

---
+++ JavaProgramming/Mini-Project/rooms.java
@@ -0,0 +1,19 @@
+public class rooms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+    }
+}
+
+class inside_room
+{   
+    int bed;
+    String nurse_assigned;
+    
+}
+class get_details extends inside_room
+{
+    int floor;
+    int Genreal_wards;
+    int icu_rooms;
+}
---
+++ JavaProjects/p3/task1/demo/target/demo/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Rohit!</h2>
+</body>
+</html>
